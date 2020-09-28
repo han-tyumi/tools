f30ec105b3b6523bb7a90461dd2c5fd89b4ad57c
han-tyumi <mmchamp95@gmail.com>
2020-09-28 02:47:46 -0400

    fix(code): :bug: update installFn import

--- installers/code/code.ts
+++ installers/code/code.ts
@@ -1,10 +1,9 @@
 import { Installer } from '../../src/installer.ts'
-
-import installFn from './code.install.ts'
+import { install } from './code.install.ts'
 
 export const codeInstaller = new Installer({
   downloadURL: (version) =>
     `https://update.code.visualstudio.com/${version}/linux-x64/stable`,
   filename: (version) => `code-${version}.tar.gz`,
-  installFn,
+  installFn: install,
 })
