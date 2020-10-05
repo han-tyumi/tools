cabc23ceaf855679ece747d8d8fb929e5a6a1557
han-tyumi <mmchamp95@gmail.com>
2020-10-04 21:02:19 -0400

    chore(module): :package: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { parse } from 'https://deno.land/std@0.71.0/encoding/yaml.ts'
-export * as fmt from 'https://deno.land/std@0.71.0/fmt/printf.ts'
-export { existsSync, moveSync } from 'https://deno.land/std@0.71.0/fs/mod.ts'
-export * as path from 'https://deno.land/std@0.71.0/path/mod.ts'
+export { parse } from 'https://deno.land/std@0.73.0/encoding/yaml.ts'
+export * as fmt from 'https://deno.land/std@0.73.0/fmt/printf.ts'
+export { existsSync, moveSync } from 'https://deno.land/std@0.73.0/fs/mod.ts'
+export * as path from 'https://deno.land/std@0.73.0/path/mod.ts'
 export {
   Command,
   HelpCommand,
   CompletionsCommand,
-} from 'https://deno.land/x/cliffy@v0.14.1/command/mod.ts'
+} from 'https://deno.land/x/cliffy@v0.14.2/command/mod.ts'
